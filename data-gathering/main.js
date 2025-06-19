582d062978d6071553865c2ec58fdc390149658b
B0nam <daniiel.bonam@gmail.com>
2025-06-18 22:04:25 -0300

    feat: add book data scrapping

--- data-gathering/main.js
+++ data-gathering/main.js
@@ -1,23 +1,32 @@
 import puppeteer from "puppeteer";
 
+const BASE_URL = "https://books.toscrape.com";
+
 (async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-    })
-    const page = await browser.newPage()
+  const browser = await puppeteer.launch();
+  const bookDataList = [];
 
-    await page.goto('https://developer.chrome.com/')
-    await page.setViewport({ width: 1080, height: 1024 })
+  for (let n = 1; n <= 50; n++) {
+    const page = await browser.newPage();
+    const url = `${BASE_URL}/catalogue/page-${n}.html`;
+    await page.goto(url);
 
-    await page.type('.devsite-search-field', 'automate beyond recorder')
+    const books = await page.$$eval('article.product_pod', books =>
+      books.map(book => {
+        const title = book.querySelector('h3 a')?.getAttribute('title');
+        const price = book.querySelector('.price_color')?.textContent;
+        const availability = book.querySelector('.instock.availability')?.innerText.trim();
+        const rating = book.classList.contains('star-rating') 
+          ? [...book.classList].find(cls => cls !== 'product_pod' && cls !== 'star-rating')
+          : 'Unknown';
+        return { title, price, availability, rating };
+      })
+    );
 
-    const searchResultSelector = '.devsite-result-item-link'
-    await page.waitForSelector(searchResultSelector)
-    await page.click(searchResultSelector);
+    bookDataList.push(...books);
+    await page.close();
+  }
 
-    const textSelector = await page.waitForSelector('text/Customize and automate')
-    const fullTitle = await textSelector?.evaluate(el => el.textContent);
-
-    console.log('The title of this blog post is "%s".', fullTitle);
-    await browser.close();
+  console.log(bookDataList);
+  await browser.close();
 })();
